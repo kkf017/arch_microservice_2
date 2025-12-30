bcb9a1b12ee5588ee1da5f0db56fe59e56676bab
kkf017 <cpunk1017@gmail.com>
2025-12-30 16:31:22 +0100

    ops  - format for Pylint

--- microservice/logging_module/handling_logger.py
+++ microservice/logging_module/handling_logger.py
@@ -4,11 +4,13 @@ import logging
 
 
 class Logger:
+    """Class to manage logging_module."""
     def __init__(
         self,
     ):
         self.logger = logging.getLogger(__name__)
         self.set_stream_handler()
+        self.handler = None
 
         self.level_debug = "%(asctime)s | \033[0;34m %(levelname)s \033[0m | %(message)s"
         self.level_info = "%(asctime)s | \033[0;32m %(levelname)s \033[0m  | %(message)s"
@@ -19,43 +21,51 @@ class Logger:
         self.date = "%Y-%m-%d %H:%M:%S"
 
     def set_level(self, level: int) -> None:
+        """Function to set level of logs."""
         self.logger.setLevel(level)
 
     def set_stream_handler(
         self,
     ) -> None:
+        """Function to print logs."""
         self.handler = logging.StreamHandler()
         self.logger.addHandler(self.handler)
 
     def set_file_handler(self, filename) -> None:
+        """Function to write logs in file .log."""
         self.handler = logging.FileHandler(filename, mode="w", encoding="utf-8")
         self.logger.addHandler(self.handler)
 
     def debug(self, msg: str) -> None:
+        """Message level debug."""
         form = logging.Formatter(self.level_debug, datefmt=self.date)
         self.handler.setFormatter(form)
         # msg = f"{filename.split('/')[-1]} | {msg}"
         self.logger.debug(msg)
 
     def info(self, msg: str) -> None:
+        """Message level info."""
         form = logging.Formatter(self.level_info, datefmt=self.date)
         self.handler.setFormatter(form)
         # msg = f"{filename.split('/')[-1]} | {msg}"
         self.logger.info(msg)
 
     def warning(self, msg: str) -> None:
+        """Message level warning."""
         form = logging.Formatter(self.level_warning, datefmt=self.date)
         self.handler.setFormatter(form)
         # msg = f"{filename.split('/')[-1]} | {msg}"
         self.logger.warning(msg)
 
     def error(self, msg: str) -> None:
+        """Message level error."""
         form = logging.Formatter(self.level_error, datefmt=self.date)
         self.handler.setFormatter(form)
         # msg = f"{filename.split('/')[-1]} | {msg}"
         self.logger.error(msg)
 
     def critical(self, msg: str) -> None:
+        """Message level critical."""
         form = logging.Formatter(self.level_critical, datefmt=self.date)
         self.handler.setFormatter(form)
         # msg = f"{filename.split('/')[-1]} | {msg}"
